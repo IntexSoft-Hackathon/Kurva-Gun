45642fd21409306692d5c4e183ec489652f22af6
vchadyuk <vadim.chadyuk@intexsoft.by>
2014-10-04 21:38:47 +0300

    Add Details View

--- football-demo/app/scripts/controllers/main.js
+++ football-demo/app/scripts/controllers/main.js
@@ -8,7 +8,8 @@ app.controller('MainCtrl', function ($scope, ngDialog) {
       template: 'views/partials/dialogs/playerDetails.html',
       className: 'ngdialog-theme-plain',
       scope: $scope,
-      showClose: true
+      showClose: true,
+      closeByDocument: true
     });
     confirm.then(function () {
 
