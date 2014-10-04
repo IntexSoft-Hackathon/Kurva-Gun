c77821e89e6ad70bd48e6b777d57a8b6ae60fc89
Sergey Shumski <sergey.shumski@intexsoft.by>
2014-10-05 00:06:06 +0400

    Add new game view

--- football-demo/app/scripts/controllers/main.js
+++ football-demo/app/scripts/controllers/main.js
@@ -2,19 +2,29 @@
 
 app.controller('MainCtrl', function ($scope, ngDialog) {
 
-  $scope.openDetails = function(){
+    $scope.openDetails = function(){
 
-    var confirm = ngDialog.openConfirm({
-      template: 'views/partials/dialogs/playerDetails.html',
-      className: 'ngdialog-theme-plain',
-      scope: $scope,
-      showClose: true,
-      closeByDocument: true
-    });
-    confirm.then(function () {
+        var confirm = ngDialog.openConfirm({
+            template: 'views/partials/dialogs/playerDetails.html',
+            className: 'ngdialog-theme-plain',
+            scope: $scope,
+            showClose: true,
+            closeByDocument: true
+        });
+        confirm.then(function () {
 
-    });
+        });
+    };
 
-  }
-
+    $scope.openNewGame = function(){
+        var newGame = ngDialog.openConfirm({
+            template: 'views/partials/dialogs/newGame.html',
+            className: 'ngdialog-theme-plain',
+            scope: $scope,
+            showClose: true,
+            closeByDocument: true
+        });
+        newGame.then(function () {
+        });
+    }
 });
