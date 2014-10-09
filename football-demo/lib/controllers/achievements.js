6e32a56ff02468fd9331545963b7bb63f9f749d2
Vladimir Semashko <vladimir.semashko.88@gmail.com>
2014-10-09 22:02:19 +0300

    Add achievements handling

--- football-demo/lib/controllers/achievements.js
+++ football-demo/lib/controllers/achievements.js
@@ -11,7 +11,6 @@ GameController.on(GameController.GAME_START_EVENT, function (game) {
 
 GameController.on(GameController.GAME_UPDATE_EVENT, function (game) {
     console.log("Calculate game update achievements");
-    GameController.emit(GameController.NEW_ACHIEVEMENT_EVENT);
 });
 
 GameController.on(GameController.GAME_END_EVENT, function (game) {
@@ -20,36 +19,72 @@ GameController.on(GameController.GAME_END_EVENT, function (game) {
 
 function checkTimedAchievements() {
     console.log("Calculate interval achievements");
-    GameController.findActiveGame(function(game){
-        if (game && game.game_status === GameController.STATUS_IN_PROGRESS)
-        {
+    GameController.findActiveGame(function (game) {
+        if (game && game.game_status === GameController.STATUS_IN_PROGRESS) {
             var minute = 1000 * 60;
-            if (new Date().getTime() - game.start_time.getTime() > minute)
-            {
+            if (new Date().getTime() - game.start_time.getTime() > minute) {
                 console.log("Game is running more then one minute");
             }
-            if (new Date().getTime() - game.start_time.getTime() > minute * 5)
-            {
-                console.log("Game is running more then five minutes");
+            if (new Date().getTime() - game.start_time.getTime() > minute * 10) {
+                console.log("Game is running more then ten minutes");
+                var achievement = {
+                    name: String,
+                    time: Date,
+                    description: String,
+                    image: String
+                };
+                achievement.name = "ПАТИ-ТУХЛЯК";
+                achievement.description = "Игра дольше 10 минут";
+                achievement.time = new Date();
+                achievement.image = "achievements/party_sucks.PNG";
+                for (var i = 0; i < game.team_white.players.length; i++) {
+                    var whitePlayer = game.team_white.players[i];
+                    var bluePlayer = game.team_blue.players[i];
+                    var whitePlayerAchievementExist = isAchievementExist(whitePlayer, achievement);
+                    var bluePlayerAchievementExist = isAchievementExist(bluePlayer, achievement);
+                    if (!whitePlayerAchievementExist) {
+                        whitePlayer.achievements.push(achievement);
+                        GameController.emit(GameController.NEW_ACHIEVEMENT_EVENT, whitePlayer, achievement);
+                    }
+                    if (!bluePlayerAchievementExist) {
+                        bluePlayer.achievements.push(achievement);
+                        GameController.emit(GameController.NEW_ACHIEVEMENT_EVENT, bluePlayer, achievement);
+                    }
+                    if (!whitePlayerAchievementExist) {
+                        achievement.user = whitePlayer;
+                        game.achievements.push(achievement);
+                    }
+                    if (!bluePlayerAchievementExist) {
+                        achievement.user = bluePlayer;
+                        game.achievements.push(achievement);
+                    }
+                }
             }
             var lastGoalTime = getLastGoalTime(game);
-            if (lastGoalTime && (new Date().getTime() -  lastGoalTime > minute))
-            {
+            if (lastGoalTime && (new Date().getTime() - lastGoalTime > minute)) {
                 console.log("More then one minute left since last goal");
             }
         }
     });
 }
 
-function getLastGoalTime(game)
-{
+function isAchievementExist(user, newAchievement) {
+    var result = false;
+    for (var i = 0; i < user.achievements.length; i++) {
+        var achievement = user.achievements[i];
+        if (newAchievement.name === achievement.name) {
+            result = true;
+            break;
+        }
+    }
+}
+
+function getLastGoalTime(game) {
     var lastGoalTime = null;
-    if (game.team_blue.goals.length > 0 )
-    {
+    if (game.team_blue.goals.length > 0) {
         lastGoalTime = game.team_blue.goals[game.team_blue.goals.length - 1].time.getTime();
     }
-    if (game.team_white.goals.length > 0 )
-    {
+    if (game.team_white.goals.length > 0) {
         var lastWhiteGoalTime = game.team_white.goals[game.team_white.goals.length - 1].time.getTime();
         lastGoalTime = lastGoalTime > lastWhiteGoalTime ? lastGoalTime : lastWhiteGoalTime;
     }
